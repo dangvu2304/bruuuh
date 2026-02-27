c5196fe4833d3e648c9d46d24f6af6c6163d8ba6
dangvu2304 <danghoangvu2304@gmail.com>
2026-02-27 13:40:58 +0700

    very meow

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import os 
 import asyncio
-
-from keep_alive import keep_alive   
+ 
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-keep_alive()
 
 intents = discord.Intents.default()
 intents.message_content = True
